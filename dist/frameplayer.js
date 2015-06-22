73bb7cb519d8ef94754aaca1ba81255d9011b307
Pablo de la Peña <p@shedcollective.org>
2015-06-22 09:08:53 +0100

    Better logging and some tidying up to logic.

--- dist/frameplayer.js
+++ dist/frameplayer.js
@@ -9,35 +9,145 @@ if (typeof window.frameplayer === "undefined") {
 }
 
 /**
- * This class represents an individual frame.
+ * This class provides debugging functionality to StreetView Animator
  * @return {Object}
  */
-window.frameplayer.frame = function() {
+window.frameplayer.debug = {
     /**
-     * Prevent scope issues by using `base` in callbacks instead of `this`
-     * @type {Object}
+     * Whether debugging is enabled or not
+     * @type {Boolean}
      */
-    var base = this;
+    enabled: false,
     // --------------------------------------------------------------------------
-    base.__construct = function() {
-        return base;
-    };
+    /**
+     * Renders a `log` call to the console
+     * @param  {String} caller The calling class
+     * @param  {Mixed}  items  The item(s) to log
+     * @return {Object}
+     */
+    log: function(caller, items) {
+        return this.execConsoleFunc("log", caller, items);
+    },
     // --------------------------------------------------------------------------
-    return base.__construct();
+    /**
+     * Renders an `info` call to the console
+     * @param  {String} caller The calling class
+     * @param  {Mixed}  items  The item(s) to log
+     * @return {Object}
+     */
+    info: function(caller, items) {
+        return this.execConsoleFunc("info", caller, items);
+    },
+    // --------------------------------------------------------------------------
+    /**
+     * Renders a `warn` call to the console
+     * @param  {String} caller The calling class
+     * @param  {Mixed}  items  The item(s) to log
+     * @return {Object}
+     */
+    warn: function(caller, items) {
+        return this.execConsoleFunc("warn", caller, items);
+    },
+    // --------------------------------------------------------------------------
+    /**
+     * Renders an `error` call to the console
+     * @param  {String} caller The calling class
+     * @param  {Mixed}  items  The item(s) to log
+     * @return {Object}
+     */
+    error: function(caller, items) {
+        return this.execConsoleFunc("error", caller, items);
+    },
+    // --------------------------------------------------------------------------
+    /**
+     * Prepares the arguments and executes the console method
+     * @param  {String} method The console method to call
+     * @param  {String} caller The calling class
+     * @param  {Mixed}  items  The item(s) to log
+     * @return {Object}
+     */
+    execConsoleFunc: function(method, caller, items) {
+        if (this.enabled && typeof console !== "undefined") {
+            var methods = [ "log", "info", "warn", "error" ];
+            for (var i = methods.length - 1; i >= 0; i--) {
+                if (methods[i] === method && typeof console[methods[i]] === "function") {
+                    var args = [ caller ];
+                    for (var x = 0; x < items.length; x++) {
+                        args.push(items[x]);
+                    }
+                    console[methods[i]].apply(console, args);
+                    break;
+                }
+            }
+        }
+        return this;
+    }
 };
 
 /**
  * This class represents an individual player.
+ * @param  {Object} options The global options object
  * @return {Object}
  */
-window.frameplayer.frame = function() {
+window.frameplayer.player = function(options) {
     /**
      * Prevent scope issues by using `base` in callbacks instead of `this`
      * @type {Object}
      */
     var base = this;
     // --------------------------------------------------------------------------
+    /**
+     * Alias to main namespace
+     * @type {Object}
+     */
+    var $FP = window.frameplayer;
+    // --------------------------------------------------------------------------
+    /**
+     * The default options object
+     * @type {Object}
+     */
+    base.options = {
+        frameRate: 12,
+        frameWidth: 600,
+        frameHeight: 300,
+        domElement: null,
+        mode: "background",
+        debug: {
+            enabled: false
+        },
+        loader: {}
+    };
+    // --------------------------------------------------------------------------
     base.__construct = function() {
+        base.options = $.extend(true, base.options, options);
+        //  Are we turning debugging on?
+        if (base.options.debug) {
+            $FP.debug.enabled = true;
+        }
+        base.log("Constructing");
+        // --------------------------------------------------------------------------
+        return base;
+    };
+    // --------------------------------------------------------------------------
+    /**
+     * Sends an item to the log, prefixing it with a string so that the class
+     * making the log is easily identifiable
+     * @param  {Mixed} item The item to log
+     * @return {Object}
+     */
+    base.log = function(item) {
+        $FP.debug.log("FP [Player]:", arguments);
+        return base;
+    };
+    // --------------------------------------------------------------------------
+    /**
+     * Sends an item to the error log, prefixing it with a string so that the class
+     * making the log is easily identifiable
+     * @param  {Mixed} item The item to log
+     * @return {Object}
+     */
+    base.error = function(item) {
+        $FP.debug.error("FP [Player]:", arguments);
         return base;
     };
     // --------------------------------------------------------------------------
--- src/player.js
+++ src/player.js
@@ -1,8 +1,9 @@
 /**
  * This class represents an individual player.
+ * @param  {Object} options The global options object
  * @return {Object}
  */
-window.frameplayer.frame = function() {
+window.frameplayer.player = function(options) {
 
     /**
      * Prevent scope issues by using `base` in callbacks instead of `this`
@@ -12,7 +13,75 @@ window.frameplayer.frame = function() {
 
     // --------------------------------------------------------------------------
 
+    /**
+     * Alias to main namespace
+     * @type {Object}
+     */
+    var $FP = window.frameplayer;
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * The default options object
+     * @type {Object}
+     */
+    base.options = {
+        'frameRate': 12,
+        'frameWidth': 600,
+        'frameHeight': 300,
+        'domElement': null,
+        'mode': 'background',
+        'debug': {
+            'enabled': false
+        },
+        'loader': {
+
+        },
+    };
+
+    // --------------------------------------------------------------------------
+
     base.__construct = function() {
+
+        base.options = $.extend(true, base.options, options);
+
+        //  Are we turning debugging on?
+        if (base.options.debug) {
+            $FP.debug.enabled = true;
+        }
+
+        base.log('Constructing');
+
+        // --------------------------------------------------------------------------
+
+        return base;
+    };
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Sends an item to the log, prefixing it with a string so that the class
+     * making the log is easily identifiable
+     * @param  {Mixed} item The item to log
+     * @return {Object}
+     */
+    base.log = function(item) {
+
+        $FP.debug.log('FP [Player]:', arguments);
+        return base;
+    };
+
+    // --------------------------------------------------------------------------
+
+    /**
+     * Sends an item to the error log, prefixing it with a string so that the class
+     * making the log is easily identifiable
+     * @param  {Mixed} item The item to log
+     * @return {Object}
+     */
+    base.error = function(item) {
+
+        $FP.debug.error('FP [Player]:', arguments);
         return base;
     };
 
